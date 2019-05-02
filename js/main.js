02974ebbb46414b871bcfc84206a4ab5db46c2bc
Chris Williams <cwill833@gmail.com>
2019-05-02 16:24:50 -0500

    this now works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function(){
     $('#button').on('click', (evt)=>{
-        console.log(evt)
+        let $inputV =  $('#skills').val();
+        let format = 
+        `<tr>
+        <td><button id='box'>x</button>
+        <td id='c'>${$inputV}</td>
+        </tr>`
+        console.log(format);
+        $('tbody').append(format);
+        $('#skills').val(' ');
     })
 });
